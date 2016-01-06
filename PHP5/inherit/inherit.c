5694d7c2114be28eead911d208fea0bcdad477e8
root <root@yunduanlocal.yunduan>
2016-01-06 05:32:26 -0500

    修复inherite 部分bug 并增加命名空间

--- PHP5/inherit/inherit.c
+++ PHP5/inherit/inherit.c
@@ -34,14 +34,45 @@ ZEND_DECLARE_MODULE_GLOBALS(inherit)
 /* True global resources - no need for thread safety here */
 static int le_inherit;
 
+
+//modify callback
+ZEND_INI_MH(inherit_fltval_modify)
+{
+	if (new_value_length == 0) {
+		return FAILURE;
+	}
+	return SUCCESS;
+}
+
+
+#include "SAPI.h" /* needed for sapi_module */
+PHP_INI_DISP(inherit_display_func)
+{
+	const char *value = ini_entry->value;
+	/* Select the current or original value as appropriate */
+	if (type == ZEND_INI_DISPLAY_ORIG &&
+		ini_entry->modified) {
+		value = ini_entry->orig_value;
+	}
+	/* Make the greeting bold (when HTML output is enabled) */
+	if (sapi_module.phpinfo_as_text) {
+		php_printf("%s", value);
+	} else {
+		php_printf("<b>%s</b>", value);
+	}
+}
 /* {{{ PHP_INI
  */
 /* Remove comments and fill if you need to have entries in php.ini */
 PHP_INI_BEGIN()
-	STD_PHP_INI_ENTRY("inherit.intval",      "42", PHP_INI_PERDIR, OnUpdateLong, global_val, zend_inherit_globals, inherit_globals)
-	STD_PHP_INI_ENTRY("inherit.hello", "hello php", PHP_INI_SYSTEM, OnUpdateString, global_str, zend_inherit_globals, inherit_globals)
-	PHP_INI_ENTRY("inherit.fltval", "0.1", PHP_INI_USER, NULL)
-	PHP_INI_ENTRY("inherit.boolval", "0", PHP_INI_ALL, NULL)
+	//STD_PHP_INI_ENTRY("inherit.intval",      "42", PHP_INI_ALL, OnUpdateLong, global_val, zend_inherit_globals, inherit_globals)
+	//STD_PHP_INI_ENTRY_EX("inherit.hello", "hello php", PHP_INI_ALL, OnUpdateString, global_str, zend_inherit_globals, inherit_globals, inherit_display_func)
+	//STD_ZEND_INI_BOOLEAN("inherit.bool", "1", PHP_INI_ALL, OnUpdateBool, bool, zend_inherit_globals, inherit_globals)
+	//STD_PHP_INI_BOOLEAN("inherit.use_namespace",   	"1", PHP_INI_SYSTEM, OnUpdateBool, use_namespace, zend_inherit_globals, inherit_globals)
+	//此处定义默认值  但php.ini级别高于此处  以上面的方式定义 会引起 core dump  待解决
+	PHP_INI_ENTRY("inherit.hello", "hello php", PHP_INI_USER, NULL)
+	PHP_INI_ENTRY("inherit.fltval", "0.12222", PHP_INI_USER, inherit_fltval_modify)
+	PHP_INI_ENTRY_EX("inherit.boolval", "1", PHP_INI_ALL, NULL, inherit_display_func)
 PHP_INI_END()
 
 
@@ -119,6 +150,7 @@ ZEND_METHOD(baby, __construct){
 	zend_class_entry *self;
 	self = Z_OBJCE_P(getThis());
 
+/*
 #if PHP_MAJOR_VERSION == 7
 	ZEND_PARSE_PARAMETERS_START(2,2)
 		Z_PARAM_ZVAL(age)
@@ -126,40 +158,49 @@ ZEND_METHOD(baby, __construct){
 		//Z_PARAM_OPTIONAL() == '|'
 	ZEND_PARSE_PARAMETERS_END();
 #else
+*/
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "zz", &age, &area) == FAILURE){
 		php_error(E_WARNING, "%s() parameters error",
 			get_active_function_name(TSRMLS_C));
 		RETURN_FALSE;
 	}
-#endif
 
 	zend_update_property(self, getThis(), ZEND_STRL("age"), age TSRMLS_CC);
 	zend_update_static_property(self, ZEND_STRL("area"), area TSRMLS_CC);
 	age = NULL;
 	area = NULL;
+/*
 #if PHP_MAJOR_VERSION == 7
 	age = zend_read_property(self, getThis(), ZEND_STRL("age"), 0, NULL);// php7 需要一个参数 zval *rv 默认传NULL
 #else
-	age = zend_read_property(self, getThis(), ZEND_STRL("age"), 0);
-#endif
+*/
+	age = zend_read_property(self, getThis(), ZEND_STRL("age"), 0 TSRMLS_CC);
 
 	if (ZVAL_IS_NULL(age)) {
 		php_error(E_WARNING, "age param Is NULL");
 		RETURN_FALSE;
 	}
 	php_var_dump(&age, 1 TSRMLS_CC);
-	area = zend_read_static_property(self, ZEND_STRL("area"), 0);
+	area = zend_read_static_property(self, ZEND_STRL("area"), 0 TSRMLS_CC);
 	if (ZVAL_IS_NULL(area)) {
+		//zend_throw_exception_ex(NULL, E_ERROR TSRMLS_CC, "age param Is NULL: %s::$area", self->name);
 		zend_error(E_ERROR, "age param Is NULL: %s::$area", self->name);
 		RETURN_FALSE;
 	}
 	php_var_dump(&area, 1 TSRMLS_CC);
 
 	const char *strval = INI_STR("inherit.hello");
-	long lval = INI_ORIG_INT("inherit.intval");//获取初始值
-	double dval = INI_FLT("inherit.fltval");//获取ini变量的值
+	const char *strvalold = INI_ORIG_STR("inherit.hello");
+	long lvalold = INI_ORIG_INT("inherit.intval");//获取初始值  INI_ORIG_* 与INI_* 相对于 php.ini 与 ini_set()
+	long lval = INI_INT("inherit.intval");
+	double dval = INI_FLT("inherit.fltval");//获取ini变量的值 ini_set()
+	double dvalold = INI_ORIG_FLT("inherit.fltval");
+	zend_bool bvalold = INI_ORIG_BOOL("inherit.boolval");
 	zend_bool bval = INI_BOOL("inherit.boolval");
-	php_printf("php_ini_set: inherit.hello: %s, inherit.intval: %d, inherit.fltval: %f, inherit.boolval: %d \n", strval, lval, dval, bval);
+	php_printf("odl php_ini_set: inherit.hello: %s, inherit.intval: %d, inherit.fltval: %.02f, inherit.boolval: %d \n",
+			strvalold, lvalold, dvalold, bvalold);
+	php_printf("php_ini_set: inherit.hello: %s, inherit.intval: %d, inherit.fltval: %.02f, inherit.boolval: %d \n",
+				strval, lval, dval, bval);
 
 	/*  silent参数：
 		0: 如果属性不存在，则抛出一个notice错误。
@@ -186,7 +227,6 @@ ZEND_FUNCTION(test_call){
 	zend_call_method_with_0_params(&obj, myclass_ce, NULL, "hello", NULL);
 	php_printf("call test_call \n");
 	zval_ptr_dtor(&obj);
-	return ;
 }
 
 //各自的zend_function_entry
@@ -213,35 +253,41 @@ const zend_function_entry baby_method[] = {
 
 PHP_MINIT_FUNCTION(inherit)
 {
+	zend_class_entry i_ce, p_ce, ce, b_ce;
 	REGISTER_INI_ENTRIES();//注册ini变量
 	//定义常量
 	/* 等价于define('Author', 'YunDuan'); */
 	REGISTER_STRINGL_CONSTANT("Author", "YunDuan", sizeof("YunDuan") - 1, CONST_PERSISTENT | CONST_CS);
 	REGISTER_STRINGL_CONSTANT("Version", PHP_INHERIT_VERSION, sizeof(PHP_INHERIT_VERSION) - 1, CONST_PERSISTENT | CONST_CS);
 
-	zend_class_entry ce, p_ce, i_ce,b_ce;
-	INIT_CLASS_ENTRY(i_ce, "i_myinterface", i_myinterface_method);
+
+	INHERIT_INIT_CLASS_ENTRY(i_ce, "inherit_i_myinterface", "inherit\\i_myinterface", i_myinterface_method);
+	//INIT_CLASS_ENTRY(i_ce, "i_myinterface", i_myinterface_method);
 	i_myinterface_ce = zend_register_internal_class(&i_ce TSRMLS_CC);
 
-	INIT_CLASS_ENTRY(p_ce, "parent_class", parent_class_method);
+	INHERIT_INIT_CLASS_ENTRY(p_ce, "inherit_parent_class", "inherit\\parent_class", parent_class_method);
+	//INIT_CLASS_ENTRY(p_ce, "parent_class", parent_class_method);
 	parent_class_ce = zend_register_internal_class(&p_ce TSRMLS_CC);
 	zend_class_implements(parent_class_ce TSRMLS_CC, 1, i_myinterface_ce);
 
 	//定义子类，使用zend_register_internal_class_ex函数
-	INIT_CLASS_ENTRY(ce, "myclass", myclass_method);
+	INHERIT_INIT_CLASS_ENTRY(ce, "inherit_myclass", "inherit\\myclass", myclass_method);
+	//INIT_CLASS_ENTRY(ce, "myclass", myclass_method);
+/*
 #if PHP_MAJOR_VERSION == 7
 	myclass_ce = zend_register_internal_class_ex(&ce, parent_class_ce);
 #else
+*/
 	myclass_ce = zend_register_internal_class_ex(&ce,parent_class_ce,"parent_class" TSRMLS_CC);
 	//注意：ZEND_ACC_FINAL是用来修饰方法的，而ZEND_ACC_FINAL_CLASS是用来修饰类的,但php7中已删除
 	myclass_ce->ce_flags |= ZEND_ACC_FINAL_CLASS;
-#endif
 
-	INIT_CLASS_ENTRY(b_ce, "baby", baby_method);
+	//INIT_CLASS_ENTRY(b_ce, "baby", baby_method);
+	INHERIT_INIT_CLASS_ENTRY(b_ce, "inherit_baby", "inherit\\baby", baby_method);
 	baby_ce = zend_register_internal_class(&b_ce TSRMLS_CC);
 	//定义类常量 静态属性
 	zend_declare_class_constant_stringl(baby_ce, ZEND_STRL("author"), ZEND_STRL("yunduan") TSRMLS_CC);
-	zend_declare_class_constant_stringl(baby_ce, ZEND_STRL("version"), ZEND_STRL(PHP_INHERIT_VERSION) TSRMLS_DC);
+	zend_declare_class_constant_stringl(baby_ce, ZEND_STRL("version"), ZEND_STRL(PHP_INHERIT_VERSION) TSRMLS_CC);
 	zend_declare_property_null(baby_ce, ZEND_STRL("area"), ZEND_ACC_PUBLIC|ZEND_ACC_STATIC TSRMLS_CC);
 	zend_declare_property_null(baby_ce, ZEND_STRL("age"), ZEND_ACC_PUBLIC TSRMLS_CC);
 
@@ -260,9 +306,11 @@ PHP_MSHUTDOWN_FUNCTION(inherit)
 
 PHP_RINIT_FUNCTION(inherit)
 {
+/*
 #if defined(COMPILE_DL_INHERIT) && defined(ZTS)
 	ZEND_TSRMLS_CACHE_UPDATE();
 #endif
+*/
 	return SUCCESS;
 }
 
@@ -304,47 +352,3 @@ ZEND_TSRMLS_CACHE_DEFINE();
 ZEND_GET_MODULE(inherit)
 #endif
 
-
-/**
- *
- *
-echo '<pre>';
-test_call();
-
-echo "<br>";
-echo "author:  ",baby::author,"<br>";
-echo "version: ",baby::version,"<br>";
-
-$obj = new baby("10", "www");
-echo "author:  ",Author,"<br>";
-echo "version: ",Version,"<br>";
-
-output:
-myclass __construct
-hello parent_class
-sizeof(area) == 5
-strlen(area) == 4
-PHP_MAJOR_VERSION: 5
-call test_call
-
-author:  yunduan
-version: 0.1.0
-string(2) "10"
-string(2) "wh"
-php_ini_set: inherit.hello: hello php, inherit.intval: 42, inherit.fltval: 0.100000, inherit.boolval: 0
-author:  YunDuan
-version: 0.1.0
-
-
-
-inherit phpinfo :
-inherit support	enabled
-Directive	Local Value	Master Value
-inherit.boolval	0	0
-inherit.fltval	0.1	0.1
-inherit.hello	hello php	hello php
-inherit.intval	42	42
- *
- *
- *
- */
